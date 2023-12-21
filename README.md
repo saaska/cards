921e295383ba9e86f029f18e88f7fd89e92c364e
saaska <26275111+saaska@users.noreply.github.com>
2023-12-21 22:06:54 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cards
+
+This is an API for a memo card-style demo, using PostgreSQL on Vercel.
---
+++ prisma/seed.ts
@@ -0,0 +1,48 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+import dotenv from 'dotenv';
+
+const prisma = new PrismaClient();
+
+const petData: Prisma.PetCreateInput[] = [
+    {
+        id: 'luna',
+        name: 'Luna',
+        category: 'kitten',
+    },
+    {
+        id: 'max',
+        name: 'Max',
+        category: 'doggie',
+    },
+    {
+        id: 'cooper',
+        name: 'Cooper',
+        category: 'reptile',
+    },
+];
+
+async function main() {
+    dotenv.config();
+
+    console.log(`Start seeding ...`);
+    for (const p of petData) {
+        // create pet if not exists
+        const pet = await prisma.pet.upsert({
+            where: { id: p.id },
+            create: p,
+            update: {},
+        });
+        console.log(`Upserted Pet with id: ${pet.id}`);
+    }
+    console.log(`Seeding finished.`);
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
